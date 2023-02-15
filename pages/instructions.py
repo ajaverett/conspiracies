66d03a2a29e634bbdd4955786ed8795866a7c970
socalkaden <mil20053@byui.edu>
2023-02-15 09:31:37 -0700

    Added Instructions Page to main file

---
+++ pages/instructions.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.title('Instructions')
+
+
+with st.container():
+    
+    
+    st.markdown("This site uses Reddit to gather all of the data. Reddit is a social media site with various\
+        forums called subreddits. Our site collects data from four subreddits specificially: Conspiracy, Politics,\
+            PoliticalDiscussion, and Freethought. To collect this data, we use Reddit's builtin API.")
+    st.markdown('''
+    To use this interactive graph outlining trends on conspiracies, it is a very simple \
+    process. First, select the topic you would like to see trends on. Then, select the query from the selected topic\
+    that you would like to see trends on. Then, select the subreddits you want to include. You can then type in a\
+    second query to see how it matches up with the first query.\
+                Lastly, you can mess with various settings on the graph, including:
+                
+                ''')
+
+    st.markdown('- Downloading graph as a PNG')
+    st.markdown('- Zooming in and out')
+    st.markdown('- Panning across graph')
+    st.markdown('- Autoscaling graph')
+    st.markdown('- Resetting the axes')
+    st.markdown('- Viewing graph in fullscreen mode')
+
+    st.markdown('Once you figure everything out, you will be a master at using our site.')
+    st.markdown('Enjoy!')
