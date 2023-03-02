3e62d286f2bf678ad44cbf58c1732710d0277338
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-03-02 15:40:15 -0700

    j

--- pages/Credits.py
+++ pages/Credits.py
@@ -8,7 +8,7 @@ container = st.container()
 with st.container():
     st.title('Credits')
     st.subheader('_Team Lead_')
-    st.write('AJ Averett - Data Science Major')
+    st.write('Aj Averett - Data Science Major')
     st.subheader('_Members_')
     st.write('Kaden Mills - Computer Science Major')
     st.write('Linda Spellman - Computer Science Major')
